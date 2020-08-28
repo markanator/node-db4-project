fc09f709c872d4264bd1db358c823b22cb1c2b9c
Mark Ambro <wowmarkambro@gmail.com>
2020-08-27 20:10:40 -0500

    � fixed all seed errors lol

--- data/seeds/1_make_recipes.js
+++ data/seeds/1_make_recipes.js
@@ -1,7 +1,6 @@
-
-exports.seed = function(knex) {
-  await knex("recipes").insert([
-	  	{ id: 1, name: "Easy Homemade Potato Chips" },
-		{ id: 2, name: "Chocolate Granola" },
-	])
+exports.seed = async function (knex) {
+	await knex('recipes').insert([
+		{ id: 1, name: 'Easy Homemade Potato Chips' },
+		{ id: 2, name: 'Chocolate Granola' },
+	]);
 };
--- data/seeds/2_make_ingredients.js
+++ data/seeds/2_make_ingredients.js
@@ -1,18 +1,17 @@
-
-exports.seed = function(knex) {
-  await knex("ingredients").insert([
-		{ id: 1, name: "russet potatoes, washed and dried" },
-		{ id: 2, name: "olive oil" },
-		{ id: 3, name: "coarse salt," },
-		{ id: 4, name: "Old Bay seasoning" },
-		{ id: 5, name: "unsalted butter" },
-		{ id: 6, name: "brown sugar" },
-		{ id: 7, name: "honey" },
-		{ id: 8, name: "pure vanilla extract" },
-		{ id: 9, name: "cocoa powder" },
-		{ id: 10, name: "salt" },
-		{ id: 11, name: "oats" },
-		{ id: 12, name: "chopped almonds" },
-		{ id: 13, name: "chocolate chunks" },
-	])
+exports.seed = async function (knex) {
+	await knex('ingredients').insert([
+		{ id: 1, name: 'russet potatoes, washed and dried' },
+		{ id: 2, name: 'olive oil' },
+		{ id: 3, name: 'coarse salt,' },
+		{ id: 4, name: 'Old Bay seasoning' },
+		{ id: 5, name: 'unsalted butter' },
+		{ id: 6, name: 'brown sugar' },
+		{ id: 7, name: 'honey' },
+		{ id: 8, name: 'pure vanilla extract' },
+		{ id: 9, name: 'cocoa powder' },
+		{ id: 10, name: 'salt' },
+		{ id: 11, name: 'oats' },
+		{ id: 12, name: 'chopped almonds' },
+		{ id: 13, name: 'chocolate chunks' },
+	]);
 };
